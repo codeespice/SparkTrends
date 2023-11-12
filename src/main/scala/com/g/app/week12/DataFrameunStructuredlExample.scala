94828514d660bfc52debb1c21aa02bceeb39911d
codeespice <codeespice@gmail.com>
2023-11-12 02:15:23 -0600

    week 12

--- src/main/scala/com/g/app/week12/DataFrameunStructuredlExample.scala
+++ src/main/scala/com/g/app/week12/DataFrameunStructuredlExample.scala
@@ -2,6 +2,7 @@ package com.g.app.week12
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions.{column, expr}
 import org.apache.spark.sql.{SaveMode, SparkSession}
 
 
@@ -12,12 +13,12 @@ object DataFrameunStructuredlExample extends App {
   sparkConf.set("spark.app.name","My application 1")
   sparkConf.set("spark.master","local[2]")
 
-val myregEx = """^(\S+) (\S+) (\S+) (\S+) """.r
+val myregEx = """^(\S+) (\S+)\t(\S+)\,(\S+)""".r
 case class Orders(order_id:Int,customer_id:Int,order_status:String)
 
   def parser(line:String)={
     line match{
-      case myregEx(order_id,data,customer_id,order_status)=>
+      case myregEx(order_id,date,customer_id,order_status)=>
         Orders(order_id.toInt,customer_id.toInt,order_status)
     }
   }
@@ -25,4 +26,11 @@ case class Orders(order_id:Int,customer_id:Int,order_status:String)
     .config(sparkConf)
     .getOrCreate()
 
+  var lines = spark.sparkContext.textFile("data/week11/orders_new.csv")
+val ordersRdd = lines.map(parser)
+
+  import spark.implicits._
+  val ordersDS= ordersRdd.toDS().cache() //toDS
+  val ordersDF= ordersRdd.toDF().cache() //toDF
+  ordersDF.select(column("order_id"),column("customer_id"),expr("concat(order_status,'_STATUS')")).show()
 }
